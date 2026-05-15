1438f0e80d060407e0de221ad49ed287a1fb6614
alexandre <carloscorreiasemedoalexandre@gmail.com>
2026-05-15 13:24:47 +0100

    Introdução do Menu de teste

--- main.c
+++ main.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <locale.h>
+#MODULO 1 
 
-int main(){ 
-    printf("Faz o teu trabalho");
-    printf(" E vc já fez o teu?");
-	printf("TESTE DE BRANCJH MODULO1");
-
-    return 0;
-}
+int main(){
+	#Menu
+	do{
+	
+	printf("\n-------------------------");
+	printf("\n-------Menu de testes----");
+	printf("\n1-Teste Ping");
+	printf("\n2-");
+	printf("\n3- ");
+	printf("\n4-");
+	printf("\n5- ");
+	printf("\n6- Sair                  ");
+	printf("\n-------------------------");
+	printf ("\n:");
+	scanf("%d",&op);
+	switch (op){
+	
+	case 1:
+		ping
+		 
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	
+	case 5:
+		break;
+		
+	case 6:
+		break;}	
+	default:
+		printf("\nValor invalido!!")
+	
+while (op!=6);};
+printf("Saindo...");
+return 0;}
